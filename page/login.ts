bd0133d1611a66c95ba78221f1a3efd948b94175
Hoadep <hoanguyenthi210303@gmail.com>
2024-03-18 17:56:11 +0700

    create repository

--- page/login.ts
+++ page/login.ts
@@ -1,27 +1,27 @@
 export class LoginPage {
   //   username_textbox: any;
+  // await page.goto('https://dev.esti-mateelectrical.bhsoft.co/login');
+  // await page.locator('input[type="email"]').click();
+  // await page.locator('input[type="email"]').fill('namnn.bhsoft@gmail.com');
+  // await page.locator('input[type="password"]').click();
+  // await page.locator('input[type="password"]').fill('BHTest123');
+  // await page.getByRole('button', { name: 'Sign in' }).click();
+
   page: any;
   email_textbox: any;
-  //   password_textbox: any;
+  password_textbox: any;
   login_button: any;
   url: any;
   constructor(page: any) {
     this.page = page;
-    this.url = page.goto("https://stage2.turbo-online.com/sign-in");
-    // this.username_textbox = page.getByPlaceholder("Tên đăng nhập");
-    // this.password_textbox = page.getByPlaceholder("Mật khẩu");
-    // this.login_button = page.getByRole("button", { name: "Quản lý" });
-    this.login_button = page.getByRole("button", { name: "Login" });
-    this.email_textbox = page.getByLabel("Email address *");
+    this.url = page.goto("https://dev.esti-mateelectrical.bhsoft.co/login");
+    this.login_button = page.getByRole("button", { name: "Sign in" });
+    this.email_textbox = page.locator('input[type="email"]');
+    this.password_textbox = page.locator('input[type="password"]');
   }
-  //   async login(username: string, password: string) {
-  //     // await this.username_textbox.fill(username);
-  //     // await this.password_textbox.fill(password);
-  //     // await this.password_textbox.fill(password);
-  //     await this.login_button.click();
-  //   }
-  async login(email: string) {
+  async login(email: string, password: string) {
     await this.email_textbox.fill(email);
+    await this.password_textbox.fill(password);
     await this.login_button.click();
   }
 }
--- tests/test/login/login.spec.ts
+++ tests/test/login/login.spec.ts
@@ -3,6 +3,5 @@ import { LoginPage } from "../../../page/login";
 test("test", async ({ page }) => {
   const loginPage = new LoginPage(page);
   await loginPage.url;
-  // await loginPage.login("0342838284", "Hoa1234xg");
-  await loginPage.login("anhdh.bhsoft+1@gmail.com");
+  await loginPage.login("namnn.bhsoft@gmail.com", "BHTest123");
 });
